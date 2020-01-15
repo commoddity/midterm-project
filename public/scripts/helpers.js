4605b8609448a91a6efbcda08656df0542bb8e86
Pascal <pascalvanleeuwen604@gmail.com>
2020-01-15 00:37:44 +0000

    Added pop in shopping cart functionality that displays price, quantity, name of item, etc.

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,7 +1,8 @@
 const getSumOfLocalStorage = () => {
   let sum = 0;
   for (let i = 0; i < window.localStorage.length; i++) {
-    let storageValue = window.localStorage.getItem(window.localStorage.key(i));
+    let storageKey = window.localStorage.key(i);
+    let storageValue = window.localStorage.getItem(storageKey);
     sum += parseInt(storageValue);
   }
   return sum;
@@ -19,7 +20,18 @@ const updateQuantity = () => {
     $(`[data-value='${storageKey}']`).find('.item-num').val(storageValue);
     const itemQtyField = Number($(`[data-value='${storageKey}']`).find('.item-num').val());
     const addToCartBtn = $(`[data-value='${storageKey}']`).find('.add-to-cart');
-    (itemQtyField) ? $(addToCartBtn).attr("disabled", false) : $(addToCartBtn).attr("disabled", true); 
+    (itemQtyField) ? $(addToCartBtn).attr("disabled", false) : $(addToCartBtn).attr("disabled", true);
     // console.log((itemQtyField))
   }
 };
+
+const getOrderTotal = () => {
+  let totalPrice = 0;
+  for (let i = 0; i < window.localStorage.length; i++) {
+    let storageKey = window.localStorage.key(i);
+    let storageValue = window.localStorage.getItem(storageKey);
+    const price = $('.price').val();
+    // totalPrice +=
+  }
+
+}
