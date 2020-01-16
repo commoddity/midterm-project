0a9da27afad7bc3fa9c26cc95cf682f49b1d176d
Pascal <pascalvanleeuwen604@gmail.com>
2020-01-16 07:47:03 +0000

    Started work on getOrder functionality.

--- repository/orders_repository.js
+++ repository/orders_repository.js
@@ -1,31 +1,57 @@
-module.exports = (db) => {
+module.exports = function (db) {
   return {
-    postOrders: (orderData) => {
-      const orderEntries = Object.entries(orderData);
-      const ordersQueryString = `
-      INSERT INTO orders (restaurant_id, user_id)
-      VALUES (1, 1);
-      `
+    getLatestOrderId: async function ()  {
       const idQuery = `
       SELECT id
       FROM orders
       ORDER BY id DESC
       LIMIT 1;
       `
+      await db.connect()
+      const res = await db.query(idQuery);
+      return res.rows[0].id;
+    },
+
+    postOrders: function () {
+      const ordersQueryString = `
+      INSERT INTO orders (restaurant_id, user_id)
+      VALUES (1, 1);
+      `
+      db.query(ordersQueryString)
+      .catch(e => console.error(e));
+    },
+
+    postOrderItems: function (orderData) {
+      const orderEntries = Object.entries(orderData);
       const orderItemsQueryString = `
       INSERT INTO order_items (menu_item_id, order_id, quantity)
       VALUES ($1, $2, $3);
       `
-      db.query(ordersQueryString)
-      .then(() => {
-        db.query(idQuery)
-        .then((lastOrder) => {
-          for (let i = 0; i < orderEntries.length; i++) {
-            const values = [Number(orderEntries[i][0]), lastOrder.rows[0].id, Number(orderEntries[i][1])];
-            db.query(orderItemsQueryString, values)
-            .catch(e => console.error(e));
-          }
-        })
+      return this.getLatestOrderId()
+      .then((lastOrder) => {
+        console.log("LASTORDER->", lastOrder)
+        for (let i = 0; i < orderEntries.length; i++) {
+          const values = [Number(orderEntries[i][0]), lastOrder, Number(orderEntries[i][1])];
+          db.query(orderItemsQueryString, values)
+          .catch(e => console.error(e));
+        }
+      })
+      .catch(e => console.error(e));
+    },
+
+    getOrder: function () {
+      const ordersQueryString = `
+      SELECT order_id, menu_items.name AS name, menu_items.price AS price, quantity, orders.time_placed AS time_placed
+      FROM order_items
+      JOIN menu_items ON menu_items.id = menu_item_id
+      JOIN orders ON orders.id = order_id
+      WHERE order_id = $1
+      `
+      const values = [this.getLatestOrderId()];
+      db.query(ordersQueryString, values)
+      .then((res) => {
+        console.log("RES.ROWS ->", res.rows);
+        return res.rows;
       })
       .catch(e => console.error(e));
     }
--- routes/orders_routes.js
+++ routes/orders_routes.js
@@ -10,17 +10,20 @@ module.exports = (ordersService) => {
 
   router.post("/checkout", (req, res) => {
     const orderData = req.body;
-    ordersService.postOrders(orderData)
+    ordersService.postOrders();
+    ordersService.postOrderItems(orderData);
   });
 
   router.get("/checkout", (req, res) => {
-    ordersService.getOrders()
-    .then(() => {
+    console.log("REQ ->", req.body)
+    console.log("RES ->", res.body)
+    return ordersService.getOrder()
+    .then((res) => {
+      console.log("GET RESPONSE", res)
       res.render("checkout")
     })
     .catch(e => console.error(e));
-  })
-
+  });
 
   return router
 };
