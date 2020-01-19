0bedb212f26d95ba94f62bf14df8828e3f2098f0
Pascal <pascalvanleeuwen604@gmail.com>
2020-01-19 03:54:04 +0000

    Pushed responsive layout code to master.

--- db/seeds/3_create_seeds_menu_items.sql.sql
+++ db/seeds/3_create_seeds_menu_items.sql.sql
@@ -1,10 +1,10 @@
 INSERT INTO menu_items (restaurant_id, name, blurb, price, image_url)
 VALUES (1, 'Eggs Benny', 'English muffin topped with prosciutto, fresh tomatoes & two poached eggs, covered in hollandaise sauce & served with hashbrowns.', 10, 'https://live.staticflickr.com/8864/18137445236_5d0a115631_b.jpg'),
 (1, 'Omelette', 'Savour our delicious omelettes! Traditional or egg-white only! All are accompanied with potatoes & your choice of toasted bread.', 9, 'https://live.staticflickr.com/206/462917140_4fb7f4834b_b.jpg'),
-(1, 'Fruit Salad', 'Juicy slices of fresh apples, melos, pineapples, oranges, grapes & mango. A sweet and healthy treat to start your day off right.', 7, 'https://live.staticflickr.com/5737/30622968353_35e06fcb52_b.jpg'),
+(1, 'Fruit Salad', 'Juicy slices of fresh apples, melons, pineapples, oranges, grapes & mango. A sweet and healthy treat to start your day off right.', 7, 'https://live.staticflickr.com/5737/30622968353_35e06fcb52_b.jpg'),
 (1, 'Chicken & Waffles', 'Deep fried chicken gizzards, livers or giblets served with delicious, freshly-made belgian waffles and drizzled in maple syrup.', 12, 'https://live.staticflickr.com/4038/5076305757_ca194b361b_b.jpg'),
 (1, 'Hashbrowns', 'Our golden-brown hash browns are made from the finest artisanal and hand-picked small potatoes & cooked just the right way. ', 5, 'https://live.staticflickr.com/5198/7159593410_43df93b462_b.jpg'),
-(1, 'Breakfast Burrito', 'Made with sliced & diced piaces of steak, eggs, potatoes & cheese. Served with a side of guac, sour cream and salsa.', 9, 'https://live.staticflickr.com/8106/8474708146_2bc8c2e139_b.jpg'),
+(1, 'Breakfast Burrito', 'Made with sliced & diced piaces of steak, eggs, potatoes & cheese. Served with a side of guacamole, sour cream and salsa.', 9, 'https://live.staticflickr.com/8106/8474708146_2bc8c2e139_b.jpg'),
 (1, 'Huevos Rancheros', 'Huevos rancheros is a classic Mexican breakfast, with fried eggs served on hot corn tortillas. Comes with guacamole and salsa.', 9, 'https://live.staticflickr.com/4184/34512229055_0d9a81647d_b.jpg'),
 (1, 'Pancakes', 'Fall under the spell of our delicious pancakes! A whole world of flavour, colour & freshness awaits you. Served with whipped cream.', 7, 'https://live.staticflickr.com/65535/48305311782_35aae64b6b_b.jpg'),
 (1, 'Fried Egg', 'These fried eggs have golden, lacy, crispy edges, which contrast beautifully with their runny yellow yolks. Great with sausages.', 3, 'https://live.staticflickr.com/2907/13916201522_7079f48d50_b.jpg');
